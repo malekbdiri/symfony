30d1291e83fb2bbb3c604ac72b08a3432fe6b092
malek <malek.bdiri@esprit.tn>
2023-10-17 16:45:05 +0100

    17-10-2023

--- src/Controller/AuthorController.php
+++ src/Controller/AuthorController.php
@@ -1,8 +1,13 @@
 <?php
 
 namespace App\Controller;
-
+use App\Entity\Author;
+use App\Form\AuthorType;
+use App\Repository\AuthorRepository;
+use Doctrine\Persistence\ManagerRegistry;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\Form\Extension\Core\Type\SubmitType;
+use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
 
@@ -15,6 +20,16 @@ class AuthorController extends AbstractController
             'controller_name' => 'AuthorController',
         ]);
     }
+
+    #[Route('/listAuthor', name: 'authors')]
+public function list(AuthorRepository $repository) {
+    $authors=$repository->findall();
+    return $this->render("author/listAuthors.html.twig",
+     array (        'tabAuthors'=>$authors
+));
+    
+
+}
 
     #[Route('/list/{var}', name: 'list_author')]
     public function listAuthor($var)
@@ -53,4 +68,79 @@ public function authorDetails($id)
         'author' => $author,
     ]);
 }
+ 
+#[Route('/add', name: 'add_author')]
+
+public function  add (ManagerRegistry $manager, Request $req)
+{   
+    $em = $manager->getManager();
+    $author=new Author();
+    $form =$this->CreateForm(AuthorType::class,$author);
+    $form->add('Save',SubmitType::class);
+    $form->handleRequest($req);
+    if ($form->isSubmitted() )
+    {
+        $em->persist($author);
+        $em->flush();
+        return $this->redirectToRoute('authors');
+    }
+    return $this->render('author/add.html.twig',['f'=>$form->createView()]);
+
 }
+    #[Route('/edit/{id}', name: 'app_edit')]
+    public function edit(AuthorRepository $repository, $id, Request $request)
+    {
+        $author = $repository->find($id);
+        $form = $this->createForm(AuthorType::class, $author);
+        $form->add('Edit', SubmitType::class);
+
+        $form->handleRequest($request);
+        if ($form->isSubmitted() && $form->isValid()) {
+            $em = $this->getDoctrine()->getManager();
+            $em->flush();
+            return $this->redirectToRoute("authors");
+        }
+
+        return $this->render('author/edit.html.twig', [
+            'f' => $form->createView(),
+        ]);
+    }
+    #[Route('/delete/{id}', name: 'app_delete')]
+    public function delete($id, AuthorRepository $repository)
+    {
+        $author = $repository->find($id);
+
+        if (!$author) {
+            throw $this->createNotFoundException('Auteur non trouvé');
+        }
+
+        $em = $this->getDoctrine()->getManager();
+        $em->remove($author);
+        $em->flush();
+
+        return $this->redirectToRoute('authors');
+    }
+
+}
+
+
+
+
+    /*
+    #[Route('/AddStatistique', name: 'app_AddStatistique')]
+
+    public function addStatistique(EntityManagerInterface $entityManager): Response
+    {
+        // Créez une instance de l'entité Author
+        $author1 = new Author();
+        $author1->setUsername("test"); // Utilisez "setUsername" pour définir le nom d'utilisateur
+        $author1->setEmail("test@gmail.com"); // Utilisez "setEmail" pour définir l'email
+
+        // Enregistrez l'entité dans la base de données
+        $entityManager->persist($author1);
+        $entityManager->flush();
+
+        return $this->redirectToRoute('authors'); // Redirigez vers la route 'app_Affiche'
+    }
+
+*/
